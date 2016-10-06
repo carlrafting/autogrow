103c9df3230607f6aed54bb9276f727868db2db2
Carl Räfting <carl.rafting@gmail.com>
2016-10-06 12:24:55 +0200

    Some basic tests...

--- tests/spec.js
+++ tests/spec.js
@@ -1,7 +1,44 @@
 describe("autogrow", function () {
-  var a;
-  
+  var a, textareas, mirrors;
+
   beforeEach(function () {
     a = autogrow;
+    a.init();
+    textareas = document.querySelectorAll('textarea');
+    mirrors = document.querySelectorAll('pre');
+  });
+
+  afterEach(function (){
+    textareas = null, mirrors = null;
+  });
+
+  describe("init", function () {
+    it("should set options if given", function () {
+      var opts = {
+        activeClass: 'fooBar'
+      };
+      a.init(opts);
+      var expected = a.options
+      expect(expected).toEqual(opts);
+    });
+    it("should use defaults if no options are given", function () {
+      var DEFAULTS = {
+        activeClass: 'autogrow-active',
+        areaClass: 'autogrow',
+        containerClass: 'autogrow-container',
+        mirrorClass: 'autogrow-mirror',
+        spanClass: 'autogrow-mirror-span'
+      };
+      a.init();
+
+      var container = document.querySelector('.'+DEFAULTS.containerClass);
+      var span = document.querySelector('.'+DEFAULTS.spanClass);
+
+      expect(textareas[0].classList.contains(DEFAULTS.areaClass)).toBe(true);
+      expect(container.classList.contains(DEFAULTS.containerClass)).toBe(true);
+      expect(container.classList.contains(DEFAULTS.activeClass)).toBe(true);
+      expect(mirrors[0].classList.contains(DEFAULTS.mirrorClass)).toBe(true);
+      expect(span.classList.contains(DEFAULTS.spanClass)).toBe(true);
+    });
   });
 });
