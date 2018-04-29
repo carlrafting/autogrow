c6476179958f254ea6e012ed67f33b59364f1b0f
Carl Räfting <carl.rafting@gmail.com>
2018-04-29 11:44:01 +0200

    Cleanup ESLint Rules

---
+++ .eslintrc.js
@@ -0,0 +1,45 @@
+module.exports = {
+  "env": {
+    "browser": true
+  },
+  "globals": {
+    "autogrow": true
+  },    
+  "extends": "eslint:recommended",
+  "rules": {
+    "indent": [
+        "error",
+        2
+    ],
+    "linebreak-style": [
+        "error",
+        "unix"
+    ],
+    "quotes": [
+        "error",
+        "single"
+    ],
+    "semi": [
+        "error",
+        "always"
+    ],
+    "strict": "error",
+    "wrap-iife": "error",
+    "yoda": "error",
+    "space-in-parens": "error",
+    "space-before-function-paren": ["error", {
+      "anonymous": "always",
+      "named": "never"
+    }],
+    "space-before-blocks": "error",
+    "radix": "warn",
+    "object-shorthand": "error",
+    "object-curly-spacing": [
+        "error",
+        "always"
+    ],
+    "array-bracket-spacing": "error",
+    "eqeqeq": "error",
+    "brace-style": "error"
+  }
+};
