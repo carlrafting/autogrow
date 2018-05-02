f55ee3303578463d6816b34ecc46561cfc94eb2b
Carl Räfting <carl.rafting@gmail.com>
2018-05-02 17:29:45 +0200

    Whitespace

--- autogrow.js
+++ autogrow.js
@@ -4,23 +4,23 @@
 (function (global) {
 
   'use strict';
-  
+
   var window = global.window;
   var document = window.document;
-  
+
   function autogrow(target, options) {
     // set options argument to empty object if undefined to prevent error
     if (!options) {
       options = {};
     }
-  
+
     if (!target || target === '') {
       return;
     }
-  
+
     var element;
     var char = target.charAt(0);
-  
+
     // get target element from DOM
     if (char === '#' || char === '.') {
       element = document.querySelector(target);
@@ -29,14 +29,14 @@
     }
     // exit if no element was found
     if (!element) return;
-  
+
     // set classnames for elements
     var areaClass = options.areaClass || 'autogrow';
     var containerClass = options.containerClass || 'autogrow-container';
     var activeClass = options.activeClass || 'autogrow-active';
     var mirrorClass = options.mirrorClass || 'autogrow-mirror';
     var spanClass = options.spanClass || 'autogrow-mirror-span';
-  
+
     // create a bunch of elements
     var frag = document.createDocumentFragment();
     var container = document.createElement('div');
@@ -50,16 +50,16 @@
     container.classList.add(activeClass);
     pre.classList.add(mirrorClass);
     span.classList.add(spanClass);
-  
+
     // get text from element
     span.textContent = element.value;
-  
+
     // html building... i guess
     pre.appendChild(span);
     pre.appendChild(br);
     //container.appendChild(element);
     container.appendChild(pre);
-  
+
     // create event listener for container
     container.addEventListener('input', function (event) {
       var t = event.target;
@@ -70,10 +70,10 @@
         });
       }
     });
-  
+
     // append container node to fragment
     frag.appendChild(container);
-  
+    
     // render fragment to DOM
     window.requestAnimationFrame(function () {
       element.parentNode.insertBefore(frag, element);
