96c2d403f6d496ef47083fc447c3a9118b3298ef
Carl Räfting <carl.rafting@gmail.com>
2018-05-03 23:19:38 +0200

    Update test files

--- tests/spec.js
+++ tests/spec.js
@@ -10,22 +10,44 @@ describe("autogrow", function () {
   });
 
   it("returns the target element if it was found", function () {
-    expect(a("textarea-one")).toBeDefined();
+    expect(a("textarea-one").element).toBeDefined();
+  });
+
+  it("returns the container, if element was found", function () {
+    expect(a("textarea-four").container).toBeDefined();
   });
 
   it("works if class selector is given", function () {
     var selector = '.hello';
     var element = document.querySelector(selector);
-    expect(a(selector)).toBe(element);
+    expect(a(selector).element.className).toEqual(element.className);
   });
 
   it("works if id selector is given", function () {
     var selector = '#textarea-two';
     var element = document.querySelector(selector);
-    expect(a(selector)).toBe(element);
+    expect(a(selector).element.id).toEqual(element.id);
+  });
+
+  it("works if options are given", function () {
+    var selector = '#textarea-three';
+    var element = document.querySelector(selector);
+    var className = 'customAreaClass';
+    expect(a(selector, {
+      areaClass: className
+    }).element.className).toEqual(className)
   });
 
   it("should fail silently if no element is given", function () {
     expect(a(null)).toBeUndefined();
+    expect(a('')).toBeUndefined();
+    expect(a()).toBeUndefined();
+  });
+  
+  it("should accept HTMLTextAreaElement as target argument", function () {
+    var textarea = document.createElement('textarea');
+    textarea.id = "foobar";
+    var expected = a(textarea).element.id;
+    expect(expected).toBe("foobar");
   });
 });
