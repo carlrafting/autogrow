e749749e9b7419e07c360a527aeedd5d00d3c6a0
Carl Räfting <carl.rafting@gmail.com>
2022-12-03 15:09:33 +0100

    add: github icon and pkg info

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,17 @@
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
+const icons = require('simple-icons/icons');
+const pkg = require("./package.json")
 
 module.exports = function(eleventyConfig) {
+    eleventyConfig.addGlobalData("pkg", () => {
+        return {
+            href: pkg.homepage,
+            version: pkg.version
+        };
+    });
+    eleventyConfig.addGlobalData("github_icon", () => {
+        return `${icons.siGithub.svg}`;
+    });
     eleventyConfig.addPlugin(syntaxHighlight);
     eleventyConfig.addPassthroughCopy("www/css");
     eleventyConfig.addPassthroughCopy("dist/autogrow.css");
