4c1faad2b0c760e37864306c599f639788ef4783
Carl Räfting <carl.rafting@gmail.com>
2022-12-04 20:24:39 +0100

    change(rollup): improve rollup config

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -6,32 +6,36 @@ const banner = `/**
  * v${pkg.version}
  */`;
 
+const esm = ({ minify = false } = {}) => ({
+  banner,
+  file: !minify ? `dist/${pkg.name}.js` : `dist/${pkg.name}.min.js`,
+  format: 'esm',
+  plugins: [minify && terser()]
+});
+
+const umd = ({ minify = false } = {}) => ({
+  banner,
+  file: !minify ? `dist/${pkg.name}.es5.js` : `dist/${pkg.name}.es5.min.js`,
+  format: 'umd',
+  exports: "named",
+  name: pkg.name,
+  plugins: [minify && terser()]
+});
+
 export default {
   input: `src/${pkg.name}.js`,
   output: [
     {
-      banner,
-      file: `dist/${pkg.name}.es5.js`,
-      format: 'umd',
-      exports: "named",
-      name: pkg.name
+      ...esm()
     },
     {
-      banner,
-      file: `dist/${pkg.name}.js`,
-      format: 'esm'
+      ...esm({ minify: true })
     },
     {
-      file: `dist/${pkg.name}.min.js`,
-      format: 'esm',
-      plugins: [terser()]
+      ...umd()
     },
     {
-      file: `dist/${pkg.name}.es5.min.js`,
-      format: 'umd',
-      exports: "named",
-      name: pkg.name,
-      plugins: [terser()]
+      ...umd({ minify: true })
     }
   ]
 };
